73925f1a6e9c74666259047111e00b9923954967
shc <shc@atguigu.com>
2023-03-27 22:40:42 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,19 @@
+package com.atguigu;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author: 孙浩诚
+ * @Create: ${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
+
+
+
