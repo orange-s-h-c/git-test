63bb232b40ceb71d9345bdd09bfedfc252682057
shc <shc@atguigu.com>
2023-03-28 10:08:03 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("put test");
     }
 }
 
