831fcc690486d6fba4ffeae9d219ace6ff677b2f
shc <61670645+orange-s-h-c@users.noreply.github.com>
2023-03-28 23:11:08 +0800

    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("put test");
+        System.out.println("pull test");
     }
 }
 
